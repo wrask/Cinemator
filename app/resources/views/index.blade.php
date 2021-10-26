a607771c108736b6b4c770e116152f9f17e8b13a
Roman Dunik <roman.dunik@intexsoft.by>
2021-10-26 17:59:28 +0300

    add initial views

---
+++ app/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <div>Index of movies</div>
+
+@endsection
--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -12,6 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .postCss('resources/css/main.css', 'public/css', [
+        require('tailwindcss'),
+    ])
+
+mix.browserSync('http://laravel.loc/');
+
